57949d385d9cf9de16cb0f3b755b734ecfbc3b6c
Rodrigo Yuri <rodrigo_yuri@hotmail.com>
2023-07-17 18:16:55 -0300

    Adicionando hover e melhorando layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,16 +6,18 @@ interface ButtonProps {
   text: string
 }
 
-export function Button({ icon: Icon, text }: ButtonProps) {
+export function Card({ icon: Icon, text }: ButtonProps) {
   return (
-    <button type="button" className="flex justify-between items-center self-stretch p-4 rounded-2xl border-2 border-slate-700 bg-slate-900">
+    <div className="flex justify-between items-center self-stretch p-4 rounded-2xl border-2 border-slate-700 bg-slate-900">
       <div className="flex items-center gap-6">
         <span className="rounded-2xl border border-slate-800 bg-slate-900 p-4">
           <Icon className="w-8 h-8 flex items-start text-slate-200 rounded-2xl gap-6 border border-slate-800 bg-slate-900" />
         </span>
         <p className="text-slate-200 text-lg font-semibold">{text}</p>
       </div>
-      <ArrowRight className="w-8 h-8 flex items-start text-slate-400" />
-    </button>
+      <button type="button" className="p-4 flex items-start rounded-2xl hover:bg-slate-950">
+        <ArrowRight className="w-8 h-8 text-slate-400" />
+      </button>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Code2, Image, PlayCircle, Music, PlusCircle, Send, Wand2 } from "lucide-react";
-import { Button } from "@/components/Button";
+import { Card } from "@/components/Card";
 
 export default function Home() {
   return (
@@ -15,10 +15,10 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col items-start gap-4 w-[38rem]">
-            <Button text="Gerador de códigos" icon={Code2} />
-            <Button text="Edição de foto" icon={Image} />
-            <Button text="Geração de vídeos" icon={PlayCircle} />
-            <Button text="Criador de áudios e musicas" icon={Music} />
+            <Card text="Gerador de códigos" icon={Code2} />
+            <Card text="Edição de foto" icon={Image} />
+            <Card text="Geração de vídeos" icon={PlayCircle} />
+            <Card text="Criador de áudios e musicas" icon={Music} />
           </div>
         </div>
 
@@ -26,11 +26,11 @@ export default function Home() {
 
         <div className="flex items-start self-stretch">
           <div className="flex items-center p-4 gap-4 flex-[1_0_0]">
-            <PlusCircle className="w-8 h-8 text-slate-400 flex items-center pl-2 absolute" />
+            <PlusCircle className="w-8 h-8 text-slate-400 flex items-center ml-4 absolute" />
 
-            <input type="text" className="flex items-center flex-[1_0_0] gap-4 p-4 rounded-2xl border-2 border-slate-800 bg-slate-900 placeholder:pl-6 placeholder:text-slate-200" placeholder="Envie uma mensagem ou digite “/” para exibir os comandos" />
+            <input type="text" className="flex items-center flex-[1_0_0] gap-4 p-4 rounded-2xl border-2 border-slate-800 bg-slate-900 placeholder:pl-6 placeholder:text-slate-200 text-slate-200" placeholder="Envie uma mensagem ou digite “/” para exibir os comandos" />
 
-            <button className="flex items-start p-4 gap-2 rounded-2xl border border-slate-700 bg-slate-900">
+            <button className="flex items-start p-4 gap-2 rounded-2xl border border-slate-700 bg-slate-900 hover:bg-slate-950">
               <Send className="text-slate-50" />
             </button>
           </div>
